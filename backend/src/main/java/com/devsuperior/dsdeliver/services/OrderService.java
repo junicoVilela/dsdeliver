9b09871afd06779a0172d14336541b7a0c1cf789
Valdemir Vilela Junior <junico.vilela@gmail.com>
2021-01-13 19:54:13 -0300

    GET Products

---
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dsdeliver.services;
+
+import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderService {
+
+    private OrderRepository orderRepository;
+}
---
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/ProductService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dsdeliver.services;
+
+import com.devsuperior.dsdeliver.dto.ProductDTO;
+import com.devsuperior.dsdeliver.entities.Product;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<ProductDTO> findAll() {
+        List<Product> list = productRepository.findAllByOrderByNameAsc();
+        return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+    }
+}
