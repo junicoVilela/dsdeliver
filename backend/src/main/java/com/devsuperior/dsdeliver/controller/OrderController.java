c3f612638ce401b942fe0c9ad934e7383cdafd51
Valdemir Vilela Junior <junico.vilela@gmail.com>
2021-01-14 14:48:00 -0300

    PUT setDelivered

--- backend/src/main/java/com/devsuperior/dsdeliver/controller/OrderController.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/controller/OrderController.java
@@ -4,7 +4,9 @@ import com.devsuperior.dsdeliver.dto.OrderDTO;
 import com.devsuperior.dsdeliver.services.OrderService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -39,4 +41,11 @@ public class OrderController {
 
         return ResponseEntity.created(uri).body(dto);
     }
+
+    @PutMapping("/{id}/delivered")
+    public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id) {
+        OrderDTO dto = orderService.setDelivered(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
 }
